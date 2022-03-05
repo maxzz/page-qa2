146cdbbfb11d889d3e53ad74ecbd4a2392871a5e
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-04 20:21:04 -0800

    Add initial other sections

--- src/components/Frontpage.tsx
+++ src/components/Frontpage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
     return (
         <div className="p-4 flex items-center justify-between bg-orange-300 shadow-sm">
-            <div className="text-2xl tracking-tighter font-light text-orange-500 uppercase scale-y-125" style={{textShadow: '1px 1px 2px #ffffffa0'}}>QA Extensions</div>
+            <div className="text-2xl tracking-tighter font-light text-orange-500 uppercase scale-y-125" style={{ textShadow: '1px 1px 2px #ffffffa0' }}>QA Extensions</div>
             <img src="./src/assets/traytools.png" alt="logo" />
         </div>
     );
@@ -17,11 +17,46 @@ function HeroImage() {
     );
 }
 
+function Summary() {
+    return (
+        <div className="">
+            Summary
+        </div>
+    );
+}
+
+function PrevVersion() {
+    return (
+        <div className="">
+            previous extension versions
+        </div>
+    );
+}
+
+function Conclusion() {
+    return (
+        <div className="">
+            <p>Browser extensions installation instructions</p>
+            <p>The documents are on the Crossmatch Confluence web site.</p>
+
+            <div className="h2">Check for duplicates</div>
+            <p>
+                Only one the DigitalPersona extension can run at the same time in the same browser.
+                After finishing (or before starting) installation of the DigitalPersona extension,
+                make sure that any previous versions of extension are uninstalled.
+            </p>
+        </div>
+    );
+}
+
 function Frontpage() {
     return (
         <div>
             <Header />
             <HeroImage />
+            <Summary />
+            <PrevVersion />
+            <Conclusion />
         </div>
     );
 }
