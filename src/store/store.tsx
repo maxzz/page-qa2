162725c846668b4f9c1dc6eee2ec748877b3feb8
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-07 21:34:24 -0800

    Add initial store definitions

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,26 +1,32 @@
 import { atom } from "jotai";
+import React from "react";
+import { IconCrLogo, IconFfLogo, IconMsLogo } from "../components/UI/UIIcons";
 
 export type LatestExtension = {
     name: string;
-    icon: string;
+    icon: React.ReactNode;
+    version: string;
     url: string;
 }
 
 export const extensionChAtom = atom<LatestExtension>({
     name: 'Chrome',
-    icon: 'chrome',
+    icon: <IconCrLogo className="w-8 h-8" />,
+    version: '3.4.430',
     url: 'chrome',
 });
 
 export const extensionFfAtom = atom<LatestExtension>({
     name: 'Firefox',
-    icon: 'chrome',
+    icon: <IconFfLogo className="w-8 h-8" />,
+    version: '3.4.430',
     url: 'chrome',
 });
 
 export const extensionMsAtom = atom<LatestExtension>({
     name: 'Edge',
-    icon: 'chrome',
+    icon: <IconMsLogo className="w-8 h-8" />,
+    version: '3.4.430',
     url: 'chrome',
 });
 
