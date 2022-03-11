8584955f76a655b37734c7533a2b89e421b727fd
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-11 15:00:43 -0800

    WIP. markdown. disconnect custom markdown render for now no need

--- src/components/sections/ReleaseNotes.tsx
+++ src/components/sections/ReleaseNotes.tsx
@@ -7,21 +7,20 @@ import { UISectionPane } from '../UI/UISectionPane';
 import { UIAccordion } from '../UI/UIAccordion';
 import './markdown.scss';
 
-const renderer = {
-    heading(text: string, level: number) {
-        const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
-        return `
-            <h${level}>
-                <a name="${escapedText}" class="anchor" href="#${escapedText}">
-                    <span class="header-link">#</span>
-                </a>
-                ${text}
-            </h${level}>
-        `;
-    }
-};
-
-marked.use({ renderer });
+// const renderer = {
+//     heading(text: string, level: number) {
+//         const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+//         return `
+//             <h${level}>
+//                 <a name="${escapedText}" class="anchor" href="#${escapedText}">
+//                     <span class="header-link">#</span>
+//                 </a>
+//                 ${text}
+//             </h${level}>
+//         `;
+//     }
+// };
+// marked.use({ renderer });
 
 export function ReleaseNotes() {
     const [releaseNotes, setReleaseNotes] = useAtom(releaseNotesAtom);
