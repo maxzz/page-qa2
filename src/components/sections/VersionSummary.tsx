b5bbc28c2dfd985f4edab4d99d25ac9200def15e
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-14 23:59:48 -0700

    WIP. VersionSummary. new filter

--- src/components/sections/VersionSummary.tsx
+++ src/components/sections/VersionSummary.tsx
@@ -2,12 +2,40 @@ import React from 'react';
 import { useAtom } from 'jotai';
 import { SectionHeader } from '../Frontpage';
 import { extInfosStateAtom } from '@/store/store';
-import { TBrandName, TBrowser } from '@/store/utils/utils-current-config';
+import { IExtnInfo, TBrand, TBrandName, TBrowser } from '@/store/utils/utils-current-config';
+
+type Table = {
+    [key in TBrowser]: {
+        [key in TBrand]?: {
+            qa?: IExtnInfo;
+            release?: IExtnInfo;
+        }
+    };
+};
+
+function reduceForTable(infos: IExtnInfo[]) {
+    return infos.reduce<Table>((acc, cur) => {
+        if (cur.browser && cur.brand) {
+            if (!acc[cur.browser]) {
+                acc[cur.browser] = {};
+            }
+            if (!acc[cur.browser][cur.brand]) {
+                acc[cur.browser][cur.brand] = {};
+            }
+            acc[cur.browser][cur.brand]![cur.qa ? 'qa' : 'release'] = cur;
+        }
+        return acc;
+    }, {} as Table);
+}
 
 export function VersionSummary() {
     const [extInfos] = useAtom(extInfosStateAtom);
     const summary = extInfos.data?.summary || [];
     const chrome = summary.filter((ext) => ext.browser === TBrowser.chrome);
+
+    const res = reduceForTable(summary);
+    console.log('table', res);
+
     return (
         <>
             <SectionHeader>
