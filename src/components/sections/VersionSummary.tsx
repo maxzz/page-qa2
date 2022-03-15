8ca32610180a8d73796dd8b0a57d8bc4e14605dc
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-14 18:00:24 -0700

    WIP. VersionSummary. add view as table

--- src/components/sections/VersionSummary.tsx
+++ src/components/sections/VersionSummary.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useAtom } from 'jotai';
 import { SectionHeader } from '../Frontpage';
+import { extInfosStateAtom } from '@/store/store';
+import { TBrandName, TBrowser } from '@/store/utils/utils-current-config';
 
 export function VersionSummary() {
+    const [extInfos] = useAtom(extInfosStateAtom);
+    const summary = extInfos.data?.summary || [];
+    const chrome = summary.filter((ext) => ext.browser === TBrowser.chrome);
     return (
-        <SectionHeader>
-            <div className="">Current verions summary table</div>
-        </SectionHeader>
+        <>
+            <SectionHeader>
+                <div className="uppercase">Current verions summary table</div>
+            </SectionHeader>
+            <div className="grid grid-cols-3">
+                <div className="">Brand</div>
+                <div className="">QA</div>
+                <div className="">Public</div>
+
+                {chrome.map((ext) => (
+                    <>
+                        <div className="">{TBrandName(ext.brand)}</div>
+                        <div className="">{ext.version}</div>
+                        <div className="">{`${ext.qa}`}</div>
+                    </>
+                ))}
+            </div>
+        </>
     );
 }
