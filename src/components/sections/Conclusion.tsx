c29c8432513cd2906e27a03ce53ac06b88145bfc
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-15 23:47:20 -0700

    WIP. add Conclusion fix layout

--- src/components/sections/Conclusion.tsx
+++ src/components/sections/Conclusion.tsx
@@ -2,21 +2,23 @@ import React from 'react';
 import { SectionHeader } from '../Frontpage';
 
 export function Conclusion() {
-    return (<>
-        <SectionHeader>
-            Conclusion
-        </SectionHeader>
+    return (
+        <div>
+            <SectionHeader>
+                Conclusion
+            </SectionHeader>
 
-        <div className="">
-            <p className="font-semibold">Browser extensions installation instructions</p>
-            <p>The documents are on <a className="underline" href="https://crossmatch.atlassian.net/wiki/spaces/ALTUS/pages/103023073/Browser+extensions+installation">the Crossmatch Confluence web site.</a></p>
+            <div className="">
+                <p className="font-semibold">Browser extensions installation instructions</p>
+                <p>The documents are on <a className="underline" href="https://crossmatch.atlassian.net/wiki/spaces/ALTUS/pages/103023073/Browser+extensions+installation">the Crossmatch Confluence web site.</a></p>
 
-            <div className="mt-4 font-semibold">Check for duplicates</div>
-            <p>
-                Only one the DigitalPersona extension can run at the same time in the same browser.
-                After finishing (or before starting) installation of the DigitalPersona extension,
-                make sure that any previous versions of extension are uninstalled.
-            </p>
+                <div className="mt-4 font-semibold">Check for duplicates</div>
+                <p>
+                    Only one the DigitalPersona extension can run at the same time in the same browser.
+                    After finishing (or before starting) installation of the DigitalPersona extension,
+                    make sure that any previous versions of extension are uninstalled.
+                </p>
+            </div>
         </div>
-    </>);
+    );
 }
