2230261771171f7680ee6398d10c16c78a155d38
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-15 23:43:16 -0700

    WIP. add Conclusion component

--- src/components/Frontpage.tsx
+++ src/components/Frontpage.tsx
@@ -5,6 +5,7 @@ import { HeroSection } from './sections/HeroSection';
 import { ReleaseNotes } from './sections/ReleaseNotes';
 import { VersionSummary } from './sections/VersionSummary';
 import { PreviousVersions } from './sections/PreviousVersions';
+import { Conclusion } from './sections/Conclusion';
 
 export function SectionHeader({ children }: React.HTMLAttributes<HTMLElement>) {
     return (
@@ -12,26 +13,6 @@ export function SectionHeader({ children }: React.HTMLAttributes<HTMLElement>) {
             {children}
         </section>
     );
-}
-
-function Conclusion() {
-    return (<>
-        <SectionHeader>
-            Conclusion
-        </SectionHeader>
-
-        <div className="">
-            <p className="font-semibold">Browser extensions installation instructions</p>
-            <p>The documents are on the Crossmatch Confluence web site.</p>
-
-            <div className="font-semibold">Check for duplicates</div>
-            <p>
-                Only one the DigitalPersona extension can run at the same time in the same browser.
-                After finishing (or before starting) installation of the DigitalPersona extension,
-                make sure that any previous versions of extension are uninstalled.
-            </p>
-        </div>
-    </>);
 }
 
 export function Frontpage() {
---
+++ src/components/sections/Conclusion.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { SectionHeader } from '../Frontpage';
+
+export function Conclusion() {
+    return (<>
+        <SectionHeader>
+            Conclusion
+        </SectionHeader>
+
+        <div className="">
+            <p className="font-semibold">Browser extensions installation instructions</p>
+            <p>The documents are on the Crossmatch Confluence web site.</p>
+
+            <div className="font-semibold">Check for duplicates</div>
+            <p>
+                Only one the DigitalPersona extension can run at the same time in the same browser.
+                After finishing (or before starting) installation of the DigitalPersona extension,
+                make sure that any previous versions of extension are uninstalled.
+            </p>
+        </div>
+    </>);
+}
