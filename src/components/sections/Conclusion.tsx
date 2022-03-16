a9f683abb36845314b8421cd1520d984cc60a759
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-16 00:05:00 -0700

    WIP. update Conclusion section layout

--- src/components/sections/Conclusion.tsx
+++ src/components/sections/Conclusion.tsx
@@ -8,19 +8,19 @@ export function Conclusion() {
                 Conclusion
             </SectionHeader>
 
-            <div className="">
+            <div className="mt-3">
                 <p className="font-semibold">Browser extensions installation instructions</p>
-                <p>The additional documents are on <a className="underline" 
+                <p>Additional documents are available on the <a className="underline" 
                     href="https://crossmatch.atlassian.net/wiki/spaces/ALTUS/pages/103023073/Browser+extensions+installation">
-                        the HID Confluence web site.
+                        HID Confluence website.
                     </a>
                 </p>
 
-                <div className="mt-4 font-semibold">Check for duplicates</div>
+                <div className="mt-4 font-semibold">Check for duplicate extensions</div>
                 <p>
-                    Only one the DigitalPersona extension can run at the same time in the same browser.
-                    After finishing (or before starting) installation of the DigitalPersona extension,
-                    make sure that any previous versions of extension are uninstalled.
+                    Only one DigitalPersona extension can run at the same time in the same browser.
+                    After completing (or before starting) the installation of the DigitalPersona extension,
+                    ensure that all previous versions of the extension are uninstalled.
                 </p>
             </div>
         </div>
