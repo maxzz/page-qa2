3bcee3fb487f55354f3b7f6ca3d545d23364c00e
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-16 00:33:43 -0700

    WIP. PreviousVersions. add fetch archive data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useAtomValue } from 'jotai';
-import { runFetchConfigAtom, runFetchReleaseNotesAtom } from './store/store';
+import { runFetchArchiveAtom, runFetchConfigAtom, runFetchReleaseNotesAtom } from './store/store';
 import { Frontpage } from './components/Frontpage';
 import { UIToaster } from './components/UI/UiToaster';
 import './App.css';
 
 function Loader() {
     useAtomValue(runFetchConfigAtom);
+    useAtomValue(runFetchArchiveAtom);
     useAtomValue(runFetchReleaseNotesAtom);
     return null;
 }
