6bdacf3741a7b96abe39a8143c77f29ca2dc6856
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-18 18:34:40 -0700

    WIP. PreviousVersions. add split by years

--- src/components/sections/PreviousVersions.tsx
+++ src/components/sections/PreviousVersions.tsx
@@ -32,7 +32,7 @@ function addDates(archive: IFnameMeta[]): Meta[] {
     let prevYear = 0;
     const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
     return archive.map((item) => {
-        const dt = new Date(item.updated.replace(/\./g, '-')+'T00:00:00');
+        const dt = new Date(item.updated.replace(/\./g, '-') + 'T00:00:00');
         const year = dt.getFullYear();
         let yearChanged = year !== prevYear;
         prevYear = year;
@@ -45,24 +45,67 @@ function addDates(archive: IFnameMeta[]): Meta[] {
     });
 }
 
+// export function PreviousVersions() {
+//     const [extArchiveState] = useAtom(extArchiveStateAtom);
+//     const archive = addDates(extArchiveState.data || []);
+//     return (
+//         <div className="">
+//             <SectionHeader>
+//                 <div className="" title="Previously released extensions">Archive</div>
+//             </SectionHeader>
+//             <div className="mt-1 text-sm">List of previously released extensions available on the HID server.</div>
+//             <div className="mt-1 columns-7 gap-4 text-xs cursor-default">
+//                 {archive.map((item, idx) => (
+//                     <React.Fragment key={idx}>
+//                         {item.yearChanged && <div className={`${idx ? 'mt-3' : ''} bg-slate-200 font-bold`}>{item.year}</div>}
+//                         <div className="text-right" title={item.date}>{item.version}</div>
+//                     </React.Fragment>
+//                 ))}
+//             </div>
+//         </div>
+//     );
+// }
+
+function splitByYears(archive: Meta[]): Record<string, Meta[]> {
+    const res: Record<string, Meta[]> = {};
+    archive.forEach((item) => {
+        if (!res[item.year]) {
+            res[item.year] = [];
+        }
+        res[item.year].push(item);
+    });
+    return res;
+}
+
 export function PreviousVersions() {
     const [extArchiveState] = useAtom(extArchiveStateAtom);
     const archive = addDates(extArchiveState.data || []);
+    const byYears = splitByYears(archive);
     return (
         <div className="">
             <SectionHeader>
                 <div className="" title="Previously released extensions">Archive</div>
             </SectionHeader>
             <div className="mt-1 text-sm">List of previously released extensions available on the HID server.</div>
-            <div className="mt-1 columns-7 gap-4 text-xs cursor-default">
+
+            <div className="mt-1 text-xs cursor-default">
+                {Object.entries(byYears).map(([year, items], idxYear) => {
+                    return items.map((item, idx) => (
+                        <div className="text-right" title={item.date} key={`${idxYear}${idx}`}>{item.version}</div>
+                    ))
+                })}
+
+            </div>
+
+
+            {/* <div className="mt-1 columns-7 gap-4 text-xs cursor-default">
                 {archive.map((item, idx) => (
                     <React.Fragment key={idx}>
                         {item.yearChanged && <div className={`${idx ? 'mt-3' : ''} bg-slate-200 font-bold`}>{item.year}</div>}
                         <div className="text-right" title={item.date}>{item.version}</div>
                     </React.Fragment>
-
                 ))}
-            </div>
+            </div> */}
         </div>
     );
 }
