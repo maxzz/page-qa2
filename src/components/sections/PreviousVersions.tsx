49a6847dec46ced04274e0e1382030ccee018bcd
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-19 15:04:02 -0700

    WIP. PreviousVersions. update class name generation

--- src/components/sections/PreviousVersions.tsx
+++ src/components/sections/PreviousVersions.tsx
@@ -26,7 +26,8 @@ function getClass(mdfile: IFnameMeta) {
 type Meta = IFnameMeta & {
     yearChanged: boolean;
     year: number;
-    date: string;
+    cls: string;
+    tooltip: string;
 };
 
 function addDates(archive: IFnameMeta[]): Meta[] {
@@ -41,8 +42,9 @@ function addDates(archive: IFnameMeta[]): Meta[] {
             ...item,
             yearChanged,
             year,
-            date: dt.toLocaleDateString('en-US', options),
-        };
+            cls: getClass(item),
+            tooltip: dt.toLocaleDateString('en-US', options),
+        } as Meta;
     });
 }
 
@@ -73,17 +75,11 @@ export function PreviousVersions() {
                         <div className="mt-2 mb-1 border-b border-slate-200 font-bold">{year}</div>
                         <div className="columns-7">
                             {items.map((item, idx) => (
-                                <a className="leading-5 flex items-center" href={getUrl(item.fname)} target="_blank" title={item.date} key={idx}>
-                                    <span className={`w-4 h-4 mr-1 ${getClass(item)} saturate-150`}></span>
+                                <a className="leading-5 flex items-center" href={getUrl(item.fname)} target="_blank" title={item.tooltip} key={idx}>
+                                    <span className={`w-4 h-4 mr-1 ${item.cls} saturate-150`}></span>
                                     <span>{item.version}</span>
                                 </a>
                             ))}
-                            {/* {items.map((item, idx) => (
-                                <div className="leading-5 flex items-center" key={`${idxYear}${idx}`}>
-                                    <span className={`w-4 h-4 mr-1 ${getClass(item)} saturate-150`}></span>
-                                    <a href={getUrl(item.fname)} target="_blank" title={item.date}>{item.version}</a>
-                                </div>
-                            ))} */}
                         </div>
                     </div>
                 )
@@ -96,4 +92,4 @@ export function PreviousVersions() {
 //TODO: accordion
 //TODO: icons - done
 //TODO: links - done
-//TODO: timeline
+//TODO: timeline - done
