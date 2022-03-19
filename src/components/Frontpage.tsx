a5361c2cdc5c2087cf509a8c0297c9f5bed42d4b
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-18 18:17:09 -0700

    WIP. set breakpoints for h-scroll and overlay for Chrome

--- src/components/Frontpage.tsx
+++ src/components/Frontpage.tsx
@@ -19,8 +19,9 @@ export function Frontpage() {
     return (
         <div className="h-screen flex flex-col">
             <AppHeader />
-            <div className="flex-1 overflow-y-auto">
-                <div className="m-auto max-w-3xl flex flex-col space-y-4">
+            <div className="flex-1 overflow-y-auto" style={{overflow: 'overlay'}}>
+            {/* <div className="flex-1 overflow-y-auto"> */}
+                <div className="m-auto max-w-sm sm:max-w-xl md:max-w-3xl lg:max-w-4xl flex flex-col space-y-4">
                 {/* <div className="m-auto w-4/5 flex flex-col space-y-4"> */}
                 {/* <div className="m-auto max-w-[80vw] flex flex-col space-y-4"> */}
                 {/* <div className="m-auto max-w-[80%] flex flex-col space-y-4"> */}
