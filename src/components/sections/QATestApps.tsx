510192e9eb5ddd0cee8cae65c4ec280f7cb10bb6
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-19 18:07:39 -0700

    WIP. QATestApps. add preview image

--- src/components/sections/QATestApps.tsx
+++ src/components/sections/QATestApps.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { SectionHeader } from '../Frontpage';
+import mainImage from '../../assets/testapps/2022-03-01_19-09-50.jpg';
 
 export function QATestApps() {
     return (
@@ -8,7 +9,10 @@ export function QATestApps() {
                 Test Applications for QA
             </SectionHeader>
 
-            <div>Links</div>
+            <div className="mt-3">
+                <img src={mainImage} width="200px" alt="test applications preview" />
+                Links
+            </div>
         </div>
     );
 }
