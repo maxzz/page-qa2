35f6dfb3ae401a3c08a436b9a5b17d69a645799d
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-20 16:16:34 -0700

    WIP. QATestApps. embed preview image

--- src/components/sections/QATestApps.tsx
+++ src/components/sections/QATestApps.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SectionHeader } from '../Frontpage';
-import mainImage from '../../assets/testapps/2022-03-01_19-09-50.jpg';
+import mainImage from '../../assets/testapps/2022-03-01_19-09-50.png';
 
 export function QATestApps() {
     return (
@@ -9,9 +9,19 @@ export function QATestApps() {
                 Test Applications for QA
             </SectionHeader>
 
-            <div className="mt-3">
-                <img src={mainImage} width="200px" alt="test applications preview" />
-                Links
+
+            <div className="mt-2 text-sm flex flex-col space-y-2">
+                <p>Here are some test web apps that you can use to test the various features of Password Manager.</p>
+
+                <div>
+                    <img className="m-auto bg-slate-300" src={mainImage} width="200px" alt="test applications preview" />
+                </div>
+
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                </ul>
             </div>
         </div>
     );
