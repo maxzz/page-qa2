0b796987c5da51e9445598bf3172c9d73f96cc45
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-20 17:10:33 -0700

    WIP. Section4_TestApps. code clean up

--- src/components/sections/Section4_TestApps.tsx
+++ src/components/sections/Section4_TestApps.tsx
@@ -1,56 +1,30 @@
 import React from 'react';
-import { SectionHeader } from '../Frontpage';
-import mainImage from '../../assets/testapps/2022-03-01_19-09-50.png';
-import { useAtom } from 'jotai';
 import { sectionQATestOpenAtom } from '@/store/store';
-import { UISectionPane } from '../UI/UISectionPane';
-import { UIAccordion } from '../UI/UIAccordion';
 import { Section } from './Section';
+import mainImage from '../../assets/testapps/2022-03-01_19-09-50.png';
 
-export function QATestApps2() {
+export function TestApps() {
     return (
-        <>
-            {/* <SectionHeader>
-                Test Applications for QA
-            </SectionHeader> */}
+        <div className="py-2 text-sm flex flex-col space-y-2">
+            <p>Here are some test web apps that you can use to test the various features of Password Manager.</p>
 
-
-            <div className="py-2 text-sm flex flex-col space-y-2">
-                <p>Here are some test web apps that you can use to test the various features of Password Manager.</p>
-
-                <div>
-                    <img className="m-auto bg-slate-300" src={mainImage} width="200px" alt="test applications preview" />
-                </div>
-
-                <ul className="ml-4 list-disc">
-                    <li><a className="underline" href="https://maxzz.github.io/test-pm-domain-logins/#" target="_blank">Test login and password change screens on the same domain</a></li>
-                    <li><a className="underline" href="https://maxzz.github.io/test-pm-second" target="_blank">Simple login screen to test login transactions</a></li>
-                    <li><a className="underline" href="https://maxzz.github.io/test-pm" target="_blank">Customizable login and password change screens</a></li>
-                </ul>
+            <div>
+                <img className="m-auto bg-slate-300" src={mainImage} width="200px" alt="test applications preview" />
             </div>
-        </>
+
+            <ul className="ml-4 list-disc">
+                <li><a className="underline" href="https://maxzz.github.io/test-pm-domain-logins/#" target="_blank">Test login and password change screens on the same domain</a></li>
+                <li><a className="underline" href="https://maxzz.github.io/test-pm-second" target="_blank">Simple login screen to test login transactions</a></li>
+                <li><a className="underline" href="https://maxzz.github.io/test-pm" target="_blank">Customizable login and password change screens</a></li>
+            </ul>
+        </div>
     );
 }
 
-// export function Section4_TestApps() {
-//     const [open, setOpen] = useAtom(sectionQATestOpenAtom);
-//     return (<div>
-//         <UISectionPane open={open} onClick={() => setOpen(v => !v)}>
-//             <div className="">
-//                 Test Applications for QA
-//             </div>
-//         </UISectionPane>
-//         <UIAccordion toggle={open}>
-//             <QATestApps2 />
-//         </UIAccordion>
-//     </div>);
-// }
-
 export function Section4_TestApps() {
-    const [open, setOpen] = useAtom(sectionQATestOpenAtom);
-    return (<div>
+    return (
         <Section openAtom={sectionQATestOpenAtom} title="Test Applications for QA">
-            <QATestApps2 />
+            <TestApps />
         </Section>
-    </div>);
+    );
 }
