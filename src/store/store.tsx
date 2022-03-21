7bbc792540ed69f4a04cba90ba0cfa096ab10fb2
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-21 14:05:37 -0700

    WIP. store. add localStorage support

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,11 +1,59 @@
 import React from "react";
-import { atom } from "jotai";
-import { LoadingDataState, loadingDataStateInit } from "@/hooks/atomsX";
+import { atom, Getter } from "jotai";
+import atomWithCallback, { LoadingDataState, loadingDataStateInit } from "@/hooks/atomsX";
 import { marked } from "marked";
 import { getCurrentConfig, IBrExtnInfos } from "./utils/utils-current-config";
 import { fetchReleaseNotes } from "./utils/utils-release-notes";
 import { IconCrLogo, IconFfLogo, IconMsLogo } from "../components/UI/UIIcons";
 import { getExistingOnServer, IFnameMeta } from "./utils/utils-existing-on-server";
+import debounce from "@/utils/debounce";
+
+//#region LocalStorage
+
+namespace Storage {
+    const KEY = 'react-page-qa2-01';
+
+    type Store = {
+        open1: boolean;
+        open2: boolean;
+        open3: boolean;
+        open4: boolean;
+        open5: boolean;
+    };
+
+    export let initialData: Store = {
+        open1: false,
+        open2: false,
+        open3: false,
+        open4: false,
+        open5: false,
+    };
+
+    function load() {
+        const s = localStorage.getItem(KEY);
+        if (s) {
+            try {
+                let obj = JSON.parse(s) as Store;
+                initialData = { ...initialData, ...obj };
+            } catch (error) {
+            }
+        }
+    }
+    load();
+
+    export const save = debounce(function _save(get: Getter) {
+        let newStore: Store = {
+            open1: get(section1_OpenReleaseNotesAtom),
+            open2: get(section2_OpenCurrentVersionsAtom),
+            open3: get(section3_OpenArchiveAtom),
+            open4: get(section4_OpenTestAppsAtom),
+            open5: get(section5_OpenFinalNotestom),
+        };
+        localStorage.setItem(KEY, JSON.stringify(newStore));
+    }, 1000);
+}
+
+//#endregion LocalStorage
 
 export type LatestExtension = {
     name: string;
@@ -129,8 +177,8 @@ runFetchArchiveAtom.onMount = (runFetch) => runFetch();
 
 //#endregion Extensions Archive on server
 
-export const section1_OpenReleaseNotesAtom = atom(false);
-export const section2_OpenCurrentVersionsAtom = atom(false);
-export const section3_OpenArchiveAtom = atom(false);
-export const section4_OpenTestAppsAtom = atom(false);
-export const section5_OpenFinalNotestom = atom(false);
+export const section1_OpenReleaseNotesAtom = atomWithCallback<boolean>(Storage.initialData.open1, ({get}) => Storage.save(get));
+export const section2_OpenCurrentVersionsAtom = atomWithCallback<boolean>(Storage.initialData.open2, ({get}) => Storage.save(get));
+export const section3_OpenArchiveAtom = atomWithCallback<boolean>(Storage.initialData.open3, ({get}) => Storage.save(get));
+export const section4_OpenTestAppsAtom = atomWithCallback<boolean>(Storage.initialData.open4, ({get}) => Storage.save(get));
+export const section5_OpenFinalNotestom = atomWithCallback<boolean>(Storage.initialData.open5, ({get}) => Storage.save(get));
