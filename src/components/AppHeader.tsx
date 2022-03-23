34b08a10c0653f63804769cf36daea36f8ffb881
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-23 15:33:46 -0700

    WIP. update AppHeader

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -5,8 +5,8 @@ export const textShadow = { textShadow: '1px 1px 2px #000' };
 
 export function AppHeader() {
     return (<>
-        <div className="p-4 flex items-center justify-between bg-[#003f82] shadow-sm cursor-default">
-            <div className="ml-3 flex items-center space-x-2">
+        <div className="px-6 py-5 flex items-center justify-between bg-[#003f82] shadow-sm cursor-default">
+            <div className="flex items-center space-x-2">
                 {/* <img src="./src/assets/traytools.png" alt="logo" /> */}
 
                 {/* <div className="w-28 px-3 py-2 flex items-center justify-center bg-white rounded-md">
@@ -16,22 +16,22 @@ export function AppHeader() {
                     QA Extensions
                 </div> */}
                 
-                <div className="w-[4.5rem] py-3 flex items-center justify-center bg-white rounded-md">
-                    <IconHIDLogo fill="#002f87" />
+                <div className="w-20 py-2 flex items-center justify-center bg-white rounded-md">
+                    <IconHIDLogo className="px-2" fill="#002f87" />
                 </div>
                 <div className="pb-1 text-3xl tracking-tighter font-light text-slate-100 uppercase whitespace-nowrap" style={textShadow}>
                     QA Extensions
                 </div>
             </div>
-            <div className="text-lg tracking-tighter font-light text-slate-100 uppercase scale-y-90 flex items-center" style={textShadow}>
-                {/* <ul className="flex items-center space-x-2">
+            {/* <div className="text-lg tracking-tighter font-light text-slate-100 uppercase scale-y-90 flex items-center" style={textShadow}>
+                <ul className="flex items-center space-x-2">
                     <li>Current</li>
                     <li>Summary</li>
                     <li>Archive</li>
                     <li>Tests</li>
                     <li>Notes</li>
-                </ul> */}
-            </div>
+                </ul>
+            </div> */}
         </div>
         <div className="h-1 bg-[#002f87]"></div>
     </>);
