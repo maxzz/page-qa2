4bdae10df2fa1310ea8a72a3acaf9a78aa191b98
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-24 17:08:58 -0700

    WIP. define global text color

--- src/components/Frontpage.tsx
+++ src/components/Frontpage.tsx
@@ -12,7 +12,7 @@ import { Section6_Footer } from './sections/Section6_Footer';
 
 export function Frontpage() {
     return (
-        <div className="h-screen flex flex-col">
+        <div className="h-screen flex flex-col text-[#001845]">
             <AppHeader />
             <div className="flex-1 overflow-y-auto" style={{ overflow: 'overlay' }}>
                 <div className="m-auto max-w-sm sm:max-w-xl md:max-w-2xl lg:max-w-4xl flex flex-col space-y-4">
