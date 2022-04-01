d9e2600bdebfac44b5b1a8efb83ef5ff758f36f3
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-01 15:00:52 -0700

    WIP. Section5_FinalNotes. add QA website history section

--- src/components/sections/Section5_FinalNotes.tsx
+++ src/components/sections/Section5_FinalNotes.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import { CONFLUENCE_URL } from '@/store/apis/constants';
+import { URL_CONFLUENCE, URL_OLD_QA_WEBSITE } from '@/store/apis/constants';
 
 export function Section5_FinalNotes() {
     return (
         <div className="py-1">
-            <h2 className="mt-1 font-semibold">Browser extensions installation instructions</h2>
-            <p className="text-sm">
-                Additional documents are available on the <a className="underline hover:text-url" href={CONFLUENCE_URL}> HID Confluence website.</a>
-            </p>
-
-            <h2 className="mt-2 font-semibold">Check for duplicate extensions</h2>
+            <h2 className="mt-1 font-semibold">Check for duplicate extension installations</h2>
             <p className="text-sm">
                 Only one DigitalPersona extension can run at the same time in the same browser.
                 After completing (or before starting) the installation of the DigitalPersona extension,
                 ensure that all previous versions of the extension are uninstalled.
+            </p>
+
+            <h2 className="mt-2 font-semibold">Browser extensions installation instructions</h2>
+            <p className="text-sm">
+                Additional documents are available on the <a className="underline hover:text-url" href={URL_CONFLUENCE} target="_blank"> HID Confluence website.</a>
+            </p>
+
+            <h2 className="mt-2 font-semibold">QA website history</h2>
+            <p className="text-sm">
+                The previous QA website is still <a className="underline hover:text-url" href={URL_OLD_QA_WEBSITE} target="_blank"> available here.</a>
             </p>
         </div>
     );
