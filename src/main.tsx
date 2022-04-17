87496dbf708b9ae79c87f61f1f1647cc4ee3ba0b
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-17 14:02:04 -0700

    Rollbacl to React 17 behaviour. problem w/ tooltip flash

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
@@ -7,4 +8,17 @@ createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
+);
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
