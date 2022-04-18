f815ef0e85724cd45e9ad59af0df706503ea8e7b
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-18 15:59:19 -0700

    WIP. Section3_Archive. update popup items layout

--- src/components/sections/Section3_Archive.tsx
+++ src/components/sections/Section3_Archive.tsx
@@ -53,7 +53,7 @@ function GroupIcons({ orderedGroup }: { orderedGroup: OrderedGroup; }) {
                             className={classNames(
                                 `w-2 h-2 sm:w-4 sm:h-4 m-px rounded-full`,
                                 getClass(groupItem.main || groupItem.debug),
-                                groupItem.main && groupItem.debug ? 'outline outline-2 outline-offset outline-green-500/50 sm:outline-green-500/30' : '',
+                                groupItem.main && groupItem.debug && 'outline outline-2 outline-offset outline-green-500/50 sm:outline-green-500/30',
                             )}
                             key={idx}
                         />
@@ -69,9 +69,9 @@ function VersionItem({ meta }: { meta?: Meta; }) {
         return null;
     }
     return (
-        <a className="flex items-center space-x-1" href={getArchiveExtensionUrl(meta.fname)}>
+        <a className="h-5 flex items-center space-x-1" href={getArchiveExtensionUrl(meta.fname)}>
             <div className={classNames(`w-4 h-4 m-px rounded-full`, getClass(meta),)} />
-            <div className="text-xs leading-5 text-url hover:underline cursor-pointer">
+            <div className="text-xs text-url hover:underline cursor-pointer">
                 {`${TBrowserName(meta.browser)} version ${meta.version}${meta.release === ReleaseType.debug ? ' debug' : ''}`}
             </div>
         </a>
@@ -125,8 +125,8 @@ function VersionItems({ items }: { items: Meta[]; }) {
 export function Section3_Archive() {
     const byYears: OneYearExts[] = [...useAtomValue(byYearsAtom)].reverse();
     return (
-        <div className="py-2">
-            <p className="text-sm">
+        <div className="py-2 text-sm">
+            <p className="">
                 List of previously released extensions that are still available on the HID server. 
                 You can download any version for testing purposes or for any other reason.
                 Click an item to download a specific version. Debug versions are protected.
@@ -145,6 +145,10 @@ export function Section3_Archive() {
                     </div>
                 ))}
             </div>
+
+            <div className="mt-2 ">Legend:</div>
+            <p className="mt-2">
+            </p>
         </div>
     );
 }
