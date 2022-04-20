4e0c3c8736133992944a81c63793a626d715d82b
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-19 19:42:25 -0700

    WIP. Section3_Archive. separate Legend into component

--- src/components/sections/Section3_Archive.tsx
+++ src/components/sections/Section3_Archive.tsx
@@ -112,6 +112,33 @@ function VersionItems({ items }: { items: Meta[]; }) {
 
 const legendBrowsers = [TBrowserShort.chrome, TBrowserShort.chrome, TBrowserShort.firefox, TBrowserShort.firefox, TBrowserShort.firefox, TBrowserShort.dev];
 
+function Legend() {
+    return (
+        <div className="mt-2 text-xs sm:text-sm">
+            <div className="mb-0 sm:mb-1">
+                Legend:
+            </div>
+            {legendBrowsers.map((br, idx) => (
+                <div className="ml-1 flex items-center space-x-2" key={idx}>
+                    <div
+                        className={classNames(
+                            `w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4 m-px rounded-full`,
+                            getExtensionIconClass(br),
+                            (idx === 1 || idx === 3) && 'extension-small-icon-outline',
+                            (idx === 4) && 'extension-small-icon-outline hue-rotate-[293deg]',
+                        )}
+                    />
+                    <div>{`${TBrowserName(br)} extension${(idx === 1 || idx === 3) ? ' with debug information' : (idx === 4) ? ' (debug version only)' : ''}`}</div>
+                </div>
+            ))}
+            <div className="ml-1 flex items-center space-x-2">
+                <div className="w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4 m-px rounded-full bg-green-600"></div>
+                <div className="">Published extensions</div>
+            </div>
+        </div>
+    );
+}
+
 export function Section3_Archive() {
     const byYears: OneYearExts[] = [...useAtomValue(byYearsAtom)].reverse();
     return (
@@ -139,29 +166,7 @@ export function Section3_Archive() {
                 ))}
             </div>
 
-            {/* Legend */}
-            <div className="mt-2 text-xs sm:text-sm">
-                <div className="mb-0 sm:mb-1">
-                    Legend:
-                </div>
-                {legendBrowsers.map((br, idx) => (
-                    <div className="ml-1 flex items-center space-x-2" key={idx}>
-                        <div
-                            className={classNames(
-                                `w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4 m-px rounded-full`,
-                                getExtensionIconClass(br),
-                                (idx === 1 || idx === 3) && 'extension-small-icon-outline',
-                                (idx === 4) && 'extension-small-icon-outline hue-rotate-[293deg]',
-                            )}
-                        />
-                        <div>{`${TBrowserName(br)} extension${(idx === 1 || idx === 3) ? ' with debug information' : (idx === 4) ? ' (debug version only)': ''}`}</div>
-                    </div>
-                ))}
-                <div className="ml-1 flex items-center space-x-2">
-                    <div className="w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4 m-px rounded-full bg-green-600"></div>
-                    <div className="">Published extensions</div>
-                </div>
-            </div>
+            <Legend />
         </div>
     );
 }
