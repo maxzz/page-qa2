05b56eb5248a0e82e1688148eaa8c1db34bf5316
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-19 20:01:04 -0700

    WIP. Section3_Archive. add to popup body public or private information

--- src/components/sections/Section3_Archive.tsx
+++ src/components/sections/Section3_Archive.tsx
@@ -74,9 +74,10 @@ function PopupVersionItem({ meta }: { meta?: Meta; }) {
 
 function PopupBody({ orderedGroup, item }: { orderedGroup: OrderedGroup; item: Meta; }) {
     return (
-        <div className="min-w-[18rem] text-sm cursor-default">
+        <div className="min-w-[20rem] text-sm cursor-default">
             <div className="pl-1 pb-1 font-bold border-b border-slate-400">
                 Versions released on {item.date}
+                {item.published && <span className="ml-0.5 text-xs font-normal text-slate-500">public</span>}
             </div>
 
             <div className="mt-2 mb-1">
