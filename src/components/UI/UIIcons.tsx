4a4fe8e919fb5a45d9b1ebde4f07e987e49cae34
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-20 18:59:42 -0700

    WIP. Section0_HeroSection. add simpified version of Chrome icon

--- src/components/UI/UIIcons.tsx
+++ src/components/UI/UIIcons.tsx
@@ -5,7 +5,7 @@ export function IconHIDLogo(props: SVGProps<SVGSVGElement> & HTMLAttributes<SVGS
     return (
         <svg fill="currentColor" viewBox="0 0 125 33.4" {...rest}>
             {title && <title>{title}</title>}
-            <path d="M79.96 33.16V.25c10.24 0 20.24-.68 30.09.2a16.09 16.09 0 0 1 15 16.47c0 8.41-5.76 15.23-15.27 16.05-9.75.84-19.58.19-29.82.19ZM96.22 3.21v27.07c6.61 1.08 9.56-.46 10.37-6.29a51.35 51.35 0 0 0 .06-13.9c-.87-6.28-3.5-7.74-10.43-6.88ZM0 32.8V.29h15.89v13.92h12V.34h16.05V32.7H28.03V19.31H16.11V32.8ZM54.24 32.86V.39h15.65v32.47Z"/>
+            <path d="M79.96 33.16V.25c10.24 0 20.24-.68 30.09.2a16.09 16.09 0 0 1 15 16.47c0 8.41-5.76 15.23-15.27 16.05-9.75.84-19.58.19-29.82.19ZM96.22 3.21v27.07c6.61 1.08 9.56-.46 10.37-6.29a51.35 51.35 0 0 0 .06-13.9c-.87-6.28-3.5-7.74-10.43-6.88ZM0 32.8V.29h15.89v13.92h12V.34h16.05V32.7H28.03V19.31H16.11V32.8ZM54.24 32.86V.39h15.65v32.47Z" />
         </svg>
     );
 }
@@ -44,21 +44,40 @@ import ICO_CH from '../../assets/icons/browsers/chrome.svg';
 import ICO_FF from '../../assets/icons/browsers/firefox.svg';
 import ICO_MS from '../../assets/icons/browsers/edge.svg';
 
-export function IconCrLogo(props: HTMLAttributes<HTMLImageElement>) {
+export function IconCrLogo2(props: HTMLAttributes<HTMLImageElement>) {
     return (
-        <img src={ICO_CH} style={{filter: 'drop-shadow(1px 1px 1px #0002)'}} alt="Chrome logo" {...props}/>
+        <img src={ICO_CH} style={{ filter: 'drop-shadow(1px 1px 1px #0002)' }} alt="Chrome logo" {...props} />
     );
 }
 
 export function IconFfLogo(props: HTMLAttributes<HTMLImageElement>) {
     return (
         // <img src={ICO_FF} style={{filter: 'hue-rotate(270deg) drop-shadow(1px 1px 1px #0002)'}} alt="Firefox logo" {...props}/>
-        <img src={ICO_FF} style={{filter: 'drop-shadow(1px 1px 1px #0002)'}} alt="Firefox logo" {...props}/>
+        <img src={ICO_FF} style={{ filter: 'drop-shadow(1px 1px 1px #0002)' }} alt="Firefox logo" {...props} />
     );
 }
 
 export function IconMsLogo(props: HTMLAttributes<HTMLImageElement>) {
     return (
-        <img src={ICO_MS} style={{filter: 'drop-shadow(1px 1px 1px #0002)'}} alt="Microsoft Edge logo" {...props}/>
+        <img src={ICO_MS} style={{ filter: 'drop-shadow(1px 1px 1px #0002)' }} alt="Microsoft Edge logo" {...props} />
+    );
+}
+
+export function IconCrLogo(props: HTMLAttributes<SVGSVGElement>) {
+    return (
+        <svg viewBox="0 0 24 24" style={{ filter: 'drop-shadow(1px 1px 1px #0002)' }} {...props}>
+            <defs>
+                <clipPath id="a">
+                    <circle cx="12" cy="11.99" r="11.2" fill="none" />
+                </clipPath>
+            </defs>
+            <path d="M7.59 14.54 12 6.9h10a11.21 11.21 0 0 0-19.41-1v8.6Z" fill="#db4437" />
+            <path d="M16.41 14.54H7.59l-5-8.6a11.22 11.22 0 0 0 8.9 17.25l4.94-4.94Z" fill="#0f9d58" />
+            <path d="m12 6.9 4.41 7.64-4.94 8.65H12A11.21 11.21 0 0 0 22 6.9Z" fill="#ffcd40" />
+            <g clipPath="url(#a)">
+                <circle cx="12" cy="11.99" r="5.09" fill="#f1f1f1" />
+                <circle cx="12" cy="11.99" r="4.07" fill="#4285f4" />
+            </g>
+        </svg>
     );
 }
