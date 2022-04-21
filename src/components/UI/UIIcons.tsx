e94a941db258be29dd12918acb1a3d09467bb66c
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-20 19:42:51 -0700

    WIP. Section0_HeroSection. fix icon filters IDs uniqueness

--- src/components/UI/UIIcons.tsx
+++ src/components/UI/UIIcons.tsx
@@ -46,18 +46,37 @@ import ICO_MS from '../../assets/icons/browsers/edge.svg';
 
 const iconShadow = { filter: 'drop-shadow(1px 1px 1px #0002)' };
 
-export function IconCrLogo2(props: HTMLAttributes<HTMLImageElement>) {
+// export function IconCrLogo2(props: HTMLAttributes<HTMLImageElement>) {
+//     return (
+//         <img src={ICO_CH} style={iconShadow} alt="Chrome logo" {...props} />
+//     );
+// }
+
+export function IconCrLogo(props: HTMLAttributes<SVGSVGElement>) {
     return (
-        <img src={ICO_CH} style={iconShadow} alt="Chrome logo" {...props} />
+        <svg viewBox="0 0 24 24" style={iconShadow} {...props}>
+            <defs>
+                <clipPath id="cr-a">
+                    <circle cx="12" cy="11.99" r="11.2" fill="none" />
+                </clipPath>
+            </defs>
+            <path d="M7.59 14.54 12 6.9h10a11.21 11.21 0 0 0-19.41-1v8.6Z" fill="#db4437" />
+            <path d="M16.41 14.54H7.59l-5-8.6a11.22 11.22 0 0 0 8.9 17.25l4.94-4.94Z" fill="#0f9d58" />
+            <path d="m12 6.9 4.41 7.64-4.94 8.65H12A11.21 11.21 0 0 0 22 6.9Z" fill="#ffcd40" />
+            <g clipPath="url(#cr-a)">
+                <circle cx="12" cy="11.99" r="5.09" fill="#f1f1f1" />
+                <circle cx="12" cy="11.99" r="4.07" fill="#4285f4" />
+            </g>
+        </svg>
     );
 }
 
-export function IconFfLogo2(props: HTMLAttributes<HTMLImageElement>) {
-    return (
-        // <img src={ICO_FF} style={{filter: 'hue-rotate(270deg) drop-shadow(1px 1px 1px #0002)'}} alt="Firefox logo" {...props}/>
-        <img src={ICO_FF} style={iconShadow} alt="Firefox logo" {...props} />
-    );
-}
+// export function IconFfLogo2(props: HTMLAttributes<HTMLImageElement>) {
+//     return (
+//         // <img src={ICO_FF} style={{filter: 'hue-rotate(270deg) drop-shadow(1px 1px 1px #0002)'}} alt="Firefox logo" {...props}/>
+//         <img src={ICO_FF} style={iconShadow} alt="Firefox logo" {...props} />
+//     );
+// }
 
 export function IconFfLogo(props: HTMLAttributes<SVGSVGElement>) {
     return (
@@ -114,32 +133,32 @@ export function IconMsLogo(props: HTMLAttributes<SVGSVGElement>) {
     return (
         <svg viewBox="0 0 24 24" style={iconShadow} {...props}>
             <defs>
-                <radialGradient id="b" cx="-306.26" cy="377.46" r="8.27" gradientTransform="matrix(1 0 0 .95 320.79 -342.22)" gradientUnits="userSpaceOnUse">
+                <radialGradient id="me-b" cx="-306.26" cy="377.46" r="8.27" gradientTransform="matrix(1 0 0 .95 320.79 -342.22)" gradientUnits="userSpaceOnUse">
                     <stop offset=".72" stopOpacity="0" />
                     <stop offset=".95" stopOpacity=".53" />
                     <stop offset="1" />
                 </radialGradient>
-                <radialGradient id="d" cx="-120.2" cy="293.6" r="12.42" gradientTransform="matrix(.15 -.99 .8 .12 -209.83 -136.08)" gradientUnits="userSpaceOnUse">
+                <radialGradient id="me-d" cx="-120.2" cy="293.6" r="12.42" gradientTransform="matrix(.15 -.99 .8 .12 -209.83 -136.08)" gradientUnits="userSpaceOnUse">
                     <stop offset=".76" stopOpacity="0" />
                     <stop offset=".95" stopOpacity=".5" />
                     <stop offset="1" />
                 </radialGradient>
-                <radialGradient id="e" cx="-171.65" cy="355.56" r="17.55" gradientTransform="matrix(-.04 1 -2.13 -.08 753.63 205.11)" gradientUnits="userSpaceOnUse">
+                <radialGradient id="me-e" cx="-171.65" cy="355.56" r="17.55" gradientTransform="matrix(-.04 1 -2.13 -.08 753.63 205.11)" gradientUnits="userSpaceOnUse">
                     <stop offset="0" stopColor="#35c1f1" />
                     <stop offset=".11" stopColor="#34c1ed" />
                     <stop offset=".23" stopColor="#2fc2df" />
                     <stop offset=".31" stopColor="#2bc3d2" />
                     <stop offset=".67" stopColor="#36c752" />
                 </radialGradient>
-                <radialGradient id="f" cx="-67.02" cy="691.29" r="8.44" gradientTransform="matrix(.28 .96 -.78 .23 579.69 -87.05)" gradientUnits="userSpaceOnUse">
+                <radialGradient id="me-f" cx="-67.02" cy="691.29" r="8.44" gradientTransform="matrix(.28 .96 -.78 .23 579.69 -87.05)" gradientUnits="userSpaceOnUse">
                     <stop offset="0" stopColor="#66eb6e" />
                     <stop offset="1" stopColor="#66eb6e" stopOpacity="0" />
                 </radialGradient>
-                <linearGradient id="a" x1="-314.8" y1="381.16" x2="-299.34" y2="381.16" gradientTransform="translate(320.79 -364.91)" gradientUnits="userSpaceOnUse">
+                <linearGradient id="me-a" x1="-314.8" y1="381.16" x2="-299.34" y2="381.16" gradientTransform="translate(320.79 -364.91)" gradientUnits="userSpaceOnUse">
                     <stop offset="0" stopColor="#0c59a4" />
                     <stop offset="1" stopColor="#114a8b" />
                 </linearGradient>
-                <linearGradient id="c" x1="-306.64" y1="374.46" x2="-316.3" y2="384.97" gradientTransform="translate(320.79 -364.91)" gradientUnits="userSpaceOnUse">
+                <linearGradient id="me-c" x1="-306.64" y1="374.46" x2="-316.3" y2="384.97" gradientTransform="translate(320.79 -364.91)" gradientUnits="userSpaceOnUse">
                     <stop offset="0" stopColor="#1b9de2" />
                     <stop offset=".16" stopColor="#1595df" />
                     <stop offset=".67" stopColor="#0680d7" />
@@ -147,36 +166,17 @@ export function IconMsLogo(props: HTMLAttributes<SVGSVGElement>) {
                 </linearGradient>
             </defs>
             <path d="M20.93 17.42a9.34 9.34 0 0 1-.91.41 9 9 0 0 1-3.11.56c-4.1 0-7.68-2.82-7.68-6.45a2.73 2.73 0 0 1 1.43-2.36C7 9.73 6 13.6 6 15.86c0 6.41 5.91 7.06 7.18 7.06a9.1 9.1 0 0 0 2.34-.4h.11a11.07 11.07 0 0 0 5.77-4.57.34.34 0 0 0-.1-.48.36.36 0 0 0-.36 0Z"
-                fill="url(#a)" />
+                fill="url(#me-a)" />
             <path d="M20.93 17.42a9.34 9.34 0 0 1-.91.41 9 9 0 0 1-3.11.56c-4.1 0-7.68-2.82-7.68-6.45a2.73 2.73 0 0 1 1.43-2.36C7 9.73 6 13.6 6 15.86c0 6.41 5.91 7.06 7.18 7.06a9.1 9.1 0 0 0 2.34-.4h.11a11.07 11.07 0 0 0 5.77-4.57.34.34 0 0 0-.1-.48.36.36 0 0 0-.36 0Z"
                 opacity=".35" fill="url(#b)" />
             <path d="M10.07 21.83a7 7 0 0 1-.45-11.63 8.39 8.39 0 0 1 1-.62A2.86 2.86 0 0 1 12 9.23 2.8 2.8 0 0 1 14.78 12s2.12-6.9-6.93-6.9a7.28 7.28 0 0 0-6.94 6.75 11.46 11.46 0 0 0 1 4.86 11.12 11.12 0 0 0 13.56 5.82 6.55 6.55 0 0 1-5.44-.7Z"
-                fill="url(#c)" />
+                fill="url(#me-c)" />
             <path d="M10.07 21.83a7 7 0 0 1-.45-11.63 8.39 8.39 0 0 1 1-.62A2.86 2.86 0 0 1 12 9.23 2.8 2.8 0 0 1 14.78 12s2.12-6.9-6.93-6.9a7.28 7.28 0 0 0-6.94 6.75 11.46 11.46 0 0 0 1 4.86 11.12 11.12 0 0 0 13.56 5.82 6.55 6.55 0 0 1-5.44-.7Z"
-                opacity=".41" fill="url(#d)" />
+                opacity=".41" fill="url(#me-d)" />
             <path d="M14.11 13.81c-.07.09-.29.21-.29.49a.81.81 0 0 0 .41.62 7.12 7.12 0 0 0 3.6.76 5.32 5.32 0 0 0 5.26-5.31 7.61 7.61 0 0 0-1-3.81A11.12 11.12 0 0 0 12 .9 11.09 11.09 0 0 0 .91 11.84C1 8.67 4.09 6.12 7.84 6.12a8.43 8.43 0 0 1 3.64.88 6.2 6.2 0 0 1 2.67 2.53 5.52 5.52 0 0 1 .63 2.56 3.11 3.11 0 0 1-.67 1.72Z"
-                fill="url(#e)" />
+                fill="url(#me-e)" />
             <path d="M14.11 13.81c-.07.09-.29.21-.29.49a.81.81 0 0 0 .41.62 7.12 7.12 0 0 0 3.6.76 5.32 5.32 0 0 0 5.26-5.31 7.61 7.61 0 0 0-1-3.81A11.12 11.12 0 0 0 12 .9 11.09 11.09 0 0 0 .91 11.84C1 8.67 4.09 6.12 7.84 6.12a8.43 8.43 0 0 1 3.64.88 6.2 6.2 0 0 1 2.67 2.53 5.52 5.52 0 0 1 .63 2.56 3.11 3.11 0 0 1-.67 1.72Z"
-                fill="url(#f)" />
-        </svg>
-    );
-}
-
-export function IconCrLogo(props: HTMLAttributes<SVGSVGElement>) {
-    return (
-        <svg viewBox="0 0 24 24" style={iconShadow} {...props}>
-            <defs>
-                <clipPath id="a">
-                    <circle cx="12" cy="11.99" r="11.2" fill="none" />
-                </clipPath>
-            </defs>
-            <path d="M7.59 14.54 12 6.9h10a11.21 11.21 0 0 0-19.41-1v8.6Z" fill="#db4437" />
-            <path d="M16.41 14.54H7.59l-5-8.6a11.22 11.22 0 0 0 8.9 17.25l4.94-4.94Z" fill="#0f9d58" />
-            <path d="m12 6.9 4.41 7.64-4.94 8.65H12A11.21 11.21 0 0 0 22 6.9Z" fill="#ffcd40" />
-            <g clipPath="url(#a)">
-                <circle cx="12" cy="11.99" r="5.09" fill="#f1f1f1" />
-                <circle cx="12" cy="11.99" r="4.07" fill="#4285f4" />
-            </g>
+                fill="url(#me-f)" />
         </svg>
     );
 }
