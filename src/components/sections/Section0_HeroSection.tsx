fb61b511e04b9c8805adde09167f3276653e3403
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-24 16:17:50 -0700

    WIP. Section4_TestApps. add image initial appearance animation

--- src/components/sections/Section0_HeroSection.tsx
+++ src/components/sections/Section0_HeroSection.tsx
@@ -8,7 +8,7 @@ import { BrowserIcon, IconClipboard, IconDownload } from '../UI/UIIcons';
 import { classNames } from '@/utils/classnames';
 import { toastSucceeded } from '../UI/UiToaster';
 import { confetti } from 'dom-confetti';
-import { a, useSpring } from '@react-spring/web';
+import { a, easings, useSpring } from '@react-spring/web';
 import HERO_IMAGE from '@/assets/frontpage/qa-header.jpg';
 
 const confettiConfig = { //https://daniel-lundin.github.io/react-dom-confetti
@@ -28,9 +28,10 @@ const confettiConfig = { //https://daniel-lundin.github.io/react-dom-confetti
 const boxShadow = { boxShadow: '0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12)', };
 
 function HeroImage() {
+    const styles = useSpring({ scale: 1, from: { scale: 2 }, config: { duration: 3000, easing: easings.easeInOutElastic } });
     return (
-        <div className="bg-slate-400" style={{ ...boxShadow, transition: "all .2s" }}>
-            <img className="h-full object-cover border border-slate-300 border-b-slate-400" src={HERO_IMAGE} alt="hero" />
+        <div className="bg-slate-400 overflow-hidden" style={{ ...boxShadow, transition: "all .2s" }}>
+            <a.img style={styles} className="h-full object-cover border border-slate-300 border-b-slate-400" src={HERO_IMAGE} alt="hero" />
         </div>
     );
 }
