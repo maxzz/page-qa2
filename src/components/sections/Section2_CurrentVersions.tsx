8142200c4f68f0c78c5ba7256aa10654fae832dd
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-24 01:28:24 -0700

    WIP. Section2_CurrentVersions. add tables separation

--- src/components/sections/Section2_CurrentVersions.tsx
+++ src/components/sections/Section2_CurrentVersions.tsx
@@ -65,20 +65,20 @@ function TableToBrowser({ browser, table = [] }: { browser: TBrowserShort; table
                 {/* <BrowserIcon browser={browser} className="w-4 h-4 pb-0.5" />
                 <div className="mb-1 text-sm font-bold">{`${TBrowserName(browser)} extensions`}</div> */}
 
-                <div className="mb-1 text-sm font-bold">{`${TBrowserName(browser)}`}</div>
+                <div className="mb-1 px-3 text-sm font-bold">{`${TBrowserName(browser)}`}</div>
                 <BrowserIcon browser={browser} className="w-3 h-3 opacity-70" />
                 <div className="mb-1 text-sm font-bold">{`extensions`}</div>
             </div>
-            <div className="grid grid-cols-3">
-                <div className="border-b text-xs">Brand</div>
-                <div className="border-b text-xs">QA</div>
-                <div className="border-b text-xs">Public</div>
+            <div className="py-1 grid grid-cols-3 bg-slate-100 shadow">
+                <div className="border-b border-slate-200 text-xs"><div className="px-3">Brand</div></div>
+                <div className="border-b border-slate-200 text-xs">QA</div>
+                <div className="border-b border-slate-200 text-xs">Public</div>
 
                 {table.map((item, idx) => (
                     <React.Fragment key={idx}>
-                        <div className={`text-xs ${idx ? 'opacity-25' : ''}`}>{TBrandName(item.brand)}</div>
-                        <div className={`text-xs ${idx ? 'opacity-25' : ''}`} title={`${item.qa?.updated ? `Updated on ${item.qa?.updated}` : ''}`}>{item.qa?.version}</div>
-                        <div className={`text-xs ${idx ? 'opacity-25' : ''}`} title={`${item.release?.updated ? `Updated on ${item.release?.updated}` : ''}`}>{item.release?.version}</div>
+                        <div className={`text-xs ${idx ? 'opacity-25' : ''} ${!idx ? 'pt-0.5' : ''}`}><div className="px-3 pt-0.5">{TBrandName(item.brand)}</div></div>
+                        <div className={`text-xs ${idx ? 'opacity-25' : ''} ${!idx ? 'pt-0.5' : ''}`} title={`${item.qa?.updated ? `Updated on ${item.qa?.updated}` : ''}`}>{item.qa?.version}</div>
+                        <div className={`text-xs ${idx ? 'opacity-25' : ''} ${!idx ? 'pt-0.5' : ''}`} title={`${item.release?.updated ? `Updated on ${item.release?.updated}` : ''}`}>{item.release?.version}</div>
                     </React.Fragment>))
                 }
             </div>
