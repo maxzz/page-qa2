e637c51e32886a68adb9ff443c83da30f4a5dad5
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-23 18:16:24 -0700

    Add PMIT link under test apps

--- src/components/sections/Section4_TestApps.tsx
+++ src/components/sections/Section4_TestApps.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import sampleAppsImg from '../../assets/testapps/2022-03-01_19-09-50.png';
+import sampleApps22Img from '../../assets/testapps/2022-05-26_19-16-14gray2.jpg';
+import samplePmitImg from '../../assets/testapps/2022-07-23_18-05-43,pmit,gray.jpg';
 
 export function Section4_TestApps() {
     return (
@@ -17,6 +19,22 @@ export function Section4_TestApps() {
                 <img className="m-auto bg-slate-300" src={sampleAppsImg} width="200px" alt="test applications preview" />
             </div>
 
+            <ul className="ml-8 list-disc">
+                <li><a className="hoverurl" href="https://maxzz.github.io/test-pm-domain-logins22" target="_blank">Page reload test with login and corresponding password change screens on the same domain</a></li>
+            </ul>
+
+            <div className="pt-3">
+                <img className="m-auto" src={sampleApps22Img} width="200px" alt="test applications preview" />
+            </div>
+
+            <ul className="ml-8 list-disc">
+                <li><a className="hoverurl" href="https://maxzz.github.io/dropzone" target="_blank">Password Manager Investigation Tool (the latest version)</a></li>
+                <li><a className="hoverurl" href="https://maxzz.github.io/dropzone" target="_blank">Password Manager Investigation Tool (version under HID domain)</a></li>
+            </ul>
+
+            <div className="pt-3">
+                <img className="m-auto" src={samplePmitImg} width="200px" alt="test applications preview" />
+            </div>
         </div>
     );
 }
