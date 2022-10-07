052aa36b8233ea5651f805e5dc5ed8c6314cec91
Max Zakharzhevskiy <maxzz@msn.com>
2022-10-07 15:58:51 -0700

    WIP. constants. update host name initialization; code clean up

--- src/store/apis/constants.ts
+++ src/store/apis/constants.ts
@@ -4,13 +4,9 @@ export const IS_HID = /hidglobal/.test(window?.location.host || '');
 
 const currentHost = (() => {
     const reG01 = /([\s\S]*)\/g01\/([\s\S]*)/;
-
     const m = (window.location.href || '').match(reG01);
-    // const m = ('https://www.hidglobal.com/sites/default/files/crossmatch/AltusAddons/g01/pageqa1/index.html').match(reG01);
     return m ? m[1] : 'https://www.hidglobal.com/sites/default/files/crossmatch/AltusAddons';
 })();
-
-//console.log('currentHostName', currentHost);
 
 export const URL_CONFLUENCE = 'https://wiki.hidglobal.com/display/ALTUS/Browser+extensions+installation';
 export const URL_OLD_QA_WEBSITE = `${currentHost}/g01/pageqa1/index.html`;
