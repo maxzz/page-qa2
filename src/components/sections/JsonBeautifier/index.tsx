ebb635560d94744c94772640db3ea19bfb3821ce
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-02 15:46:45 -0800

    WIP. NewFTP. add jsonBeautifier state

--- src/components/sections/JsonBeautifier/index.tsx
+++ src/components/sections/JsonBeautifier/index.tsx
@@ -2,21 +2,27 @@ import { useState } from "react";
 import { beautify } from "./beautify";
 
 export function JsonBeautifier() {
+    const [open, setOpen] = useState(false);
     const [text, setText] = useState('');
     const [formatted, setFormatted] = useState('');
     return (
-        <div className="">
-            <div className="">
-                <input
-                    type="text"
-                    value={text}
-                    onChange={(e) => {
-                        const t = e.target.value;
-                        setText(t);
-                        setFormatted(beautify(t, null, 4));
-                    }} />
-                <div className="">{formatted}</div>
-            </div>
-        </div>
+        <div className="" onClick={() => setOpen((v) => !v)}>
+            <button className="">JSON beautifier...</button>
+            {open &&
+                <div className="">
+                    <input
+                        type="text"
+                        value={text}
+                        onChange={(e) => {
+                            const t = e.target.value;
+                            setText(t);
+                            setFormatted(beautify(t, null, 4));
+                        }} />
+                    <div className="">
+                        {formatted}
+                    </div>
+                </div>
+            }
+        </div >
     );
 }
