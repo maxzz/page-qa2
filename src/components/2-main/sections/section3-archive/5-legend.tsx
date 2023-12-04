7f9f5d049324dc4815b83020c5c126652dab3189
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-03 16:28:36 -0800

    WIP. review legend component

--- src/components/2-main/sections/section3-archive/5-legend.tsx
+++ src/components/2-main/sections/section3-archive/5-legend.tsx
@@ -2,8 +2,16 @@ import { TBrowserName, TBrowserShort } from '@/store/apis';
 import { classNames } from '@/utils/classnames';
 import { IconBrowser } from '@/components/ui/icons';
 
+const legendBrowsers = [
+    TBrowserShort.chrome,   // 0. Chrome extension
+    TBrowserShort.chrome,   // 1. Chrome extension with debug information
+    TBrowserShort.firefox,  // 2. Firefox extension
+    TBrowserShort.firefox,  // 3. Firefox extension with debug information
+    TBrowserShort.firefox,  // 4. Firefox extension (debug version only)
+    TBrowserShort.dev       // 5. Dev extension with binaries
+];
+
 export function Legend() {
-    const legendBrowsers = [TBrowserShort.chrome, TBrowserShort.chrome, TBrowserShort.firefox, TBrowserShort.firefox, TBrowserShort.firefox, TBrowserShort.dev];
     return (
         <div className="mt-2 text-xs sm:text-sm">
             <div className="mb-0 sm:mb-1">
@@ -13,9 +21,9 @@ export function Legend() {
                 <div className="ml-1 flex items-center space-x-2" key={idx}>
                     <IconBrowser browser={br}
                         className={classNames(
-                            `w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4 m-px rounded-full`,
-                            (idx === 1 || idx === 3) && 'extension-small-icon-outline',
-                            (idx === 4) && 'extension-small-icon-outline hue-rotate-[293deg]',
+                            "w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4 m-px rounded-full",
+                            (idx === 1 || idx === 3 || idx === 4) && "extension-small-icon-outline",
+                            (idx === 4) && "hue-rotate-[293deg]",
                         )}
                     />
                     <div>{`${TBrowserName(br)} extension${(idx === 1 || idx === 3) ? ' with debug information' : (idx === 4) ? ' (debug version only)' : ''}`}</div>
