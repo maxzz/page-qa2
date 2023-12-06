cecbd185f87852bff474174f8b5879c7f15052ed
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-05 17:55:29 -0800

    WIP. chrome3. Legend review

--- src/components/2-main/sections/section3-archive/6-legend.tsx
+++ src/components/2-main/sections/section3-archive/6-legend.tsx
@@ -5,11 +5,20 @@ import { IconBrowser } from '@/components/ui/icons';
 const legendBrowsers = [
     TBrowserShort.chrome,   // 0. Chrome extension
     TBrowserShort.chrome,   // 1. Chrome extension with debug information
-    TBrowserShort.firefox,  // 2. Firefox extension
-    TBrowserShort.firefox,  // 3. Firefox extension with debug information
-    TBrowserShort.firefox,  // 4. Firefox extension (debug version only)
-    TBrowserShort.dev       // 5. Dev extension with binaries
+    TBrowserShort.chrome3,  // 2. Chrome extension v3
+    TBrowserShort.chrome3,  // 3. Chrome extension v3 with debug information
+    TBrowserShort.firefox,  // 4. Firefox extension
+    TBrowserShort.firefox,  // 5. Firefox extension with debug information
+    TBrowserShort.firefox,  // 6. Firefox extension (debug version only)
+    TBrowserShort.dev       // 7. Dev extension with binaries
 ];
+
+const withDebugText = (idx: number) => idx === 1 || idx === 3 || idx === 5;
+const withDebugCyrcle = (idx: number) => idx === 1 || idx === 3 || idx === 5 || idx === 6;
+
+function iconText(br: TBrowserShort, idx: number) {
+    return `${convTBrowserShort2Name(br)} extension${withDebugText(idx) ? ' with debug information' : (idx === 6) ? ' (debug version only)' : ''}`;
+}
 
 export function Legend() {
     return (
@@ -22,11 +31,11 @@ export function Legend() {
                     <IconBrowser browser={br}
                         className={classNames(
                             "w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4 m-px rounded-full",
-                            (idx === 1 || idx === 3 || idx === 4) && "extension-small-icon-outline",
-                            (idx === 4) && "hue-rotate-[293deg]",
+                            withDebugCyrcle(idx) && "extension-small-icon-outline",
+                            (idx === 6) && "hue-rotate-[293deg]",
                         )}
                     />
-                    <div>{`${convTBrowserShort2Name(br)} extension${(idx === 1 || idx === 3) ? ' with debug information' : (idx === 4) ? ' (debug version only)' : ''}`}</div>
+                    <div>{iconText(br, idx)}</div>
                 </div>
             ))}
             <div className="ml-1 flex items-center space-x-2">
