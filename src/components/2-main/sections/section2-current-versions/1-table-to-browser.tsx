d13fb70508fd7fef62db7eacb0900a4f6ea9f908
Max Zakharzhevskiy <maxzz@msn.com>
2023-12-06 17:14:54 -0800

    WIP. chrome3. TableToBrowser update tables

--- src/components/2-main/sections/section2-current-versions/1-table-to-browser.tsx
+++ src/components/2-main/sections/section2-current-versions/1-table-to-browser.tsx
@@ -4,34 +4,37 @@ import { FlatTableItem } from './0-reduce-utils';
 import { IconBrowser } from '@/components/ui/icons';
 import { beautifyDate } from '@/utils/helpers';
 
+const tableHeaderClasses = "pb-1 text-xs border-slate-300 border-b";
+const tableItemClasses = (idx: number) => `text-xs ${!idx ? '' : 'opacity-25'}`;
+
 function VersionItem({ idx, item }: { idx: number; item: ExtnFromConfig | undefined; }) {
     return (
-        <div className={tableItemClasses(idx)} title={`Updated on ${beautifyDate(item?.updated)}`}>
-            {item?.version}
+        <div className="flex items-center space-x-0.5">
+            <IconBrowser browser={item?.broIcon} className="w-3 h-3 opacity-70" />
+            <div className={tableItemClasses(idx)} title={`Updated on ${beautifyDate(item?.updated)}`}>
+                {item?.version}
+            </div>
         </div>
     );
 }
-
-const tableHeaderClasses = "border-b border-slate-200 text-xs";
-const tableItemClasses = (idx: number) => `text-xs ${!idx ? 'pt-0.5' : 'opacity-25'}`;
 
 export function TableToBrowser({ browser, table = [] }: { browser: TBrowserShort; table: FlatTableItem[]; }) {
     return (
         <div className="cursor-default">
 
             {/* Table caption */}
-            <div className="flex items-center space-x-1">
-                <div className="mb-1 pl-3 text-sm font-bold">
+            <div className="flex items-center space-x-1 ">
+                <div className="mb-1 pl-3 text-sm font-semibold">
                     {`${convTBrowserShort2Name(browser)}`}
                 </div>
                 <IconBrowser browser={browser} className="w-3 h-3 opacity-70" />
-                <div className="mb-1 text-sm font-bold">
+                <div className="mb-1 text-sm font-semibold">
                     {`extensions`}
                 </div>
             </div>
 
             {/* Table columns: Brand, QA, Public */}
-            <div className="py-1 grid grid-cols-3 bg-slate-100 shadow">
+            <div className="py-1 bg-slate-50 border-slate-300 border rounded shadow overflow-hidden grid grid-cols-3 gap-y-0.5">
                 {/* Header */}
                 <div className={tableHeaderClasses}>
                     <div className="px-3">Brand</div>
@@ -50,16 +53,8 @@ export function TableToBrowser({ browser, table = [] }: { browser: TBrowserShort
                             <div className={tableItemClasses(idx)}>
                                 <div className="px-3 pt-0.5">{convTBrand2Name(item.brand)}</div>
                             </div>
-
                             <VersionItem idx={idx} item={item.qa} />
-                            {/* <div className={tableItemClasses(idx)} title={`Updated on ${beautifyDate(item.qa?.updated)}`}>
-                                {item.qa?.version}
-                            </div> */}
-
                             <VersionItem idx={idx} item={item.release} />
-                            {/* <div className={tableItemClasses(idx)} title={`Updated on ${beautifyDate(item.release?.updated)}`}>
-                                {item.release?.version}
-                            </div> */}
                         </Fragment>
                     )
                 )}
