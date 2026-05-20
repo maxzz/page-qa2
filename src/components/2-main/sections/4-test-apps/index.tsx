86119b15b0e791f00ab96f4b984b4705f2352be9
Max Zakharzhevskiy <maxzz@msn.com>
2026-05-20 15:31:34 -0700

    WIP. test-applications. update order of props

--- src/components/2-main/sections/4-test-apps/index.tsx
+++ src/components/2-main/sections/4-test-apps/index.tsx
@@ -6,11 +6,11 @@ import sampleApps22Img from '@/assets/testapps/2022-05-26_19-16-14gray2.jpg';
 import samplePmitImg from '@/assets/testapps/2022-07-23_18-05-43,pmit,gray.jpg';
 
 interface TestAppLinkProps {
-    href: string;
     label: ReactNode;
+    href: string;
 }
 
-function TestAppLink({ href, label }: TestAppLinkProps) {
+function TestAppLink({ label, href }: TestAppLinkProps) {
     return (
         <li>
             <a className="hoverurl" href={href} target="_blank">
@@ -28,29 +28,29 @@ export function Section4_TestApps() {
 
             {/*  */}
             <ul className="ml-8 list-disc">
-                <TestAppLink href="https://maxzz.github.io/test-pm-domain-logins/#" label="Two test logins and corresponding password change screens in the same domain" />
-                <TestAppLink href="https://maxzz.github.io/test-pm-second" label="Simple test login screen to verify login transactions" />
+                <TestAppLink label="Two test logins and corresponding password change screens in the same domain" href="https://maxzz.github.io/test-pm-domain-logins/#" />
+                <TestAppLink label="Simple test login screen to verify login transactions" href="https://maxzz.github.io/test-pm-second" />
             </ul>
 
             {/*  */}
             <br />
             <ul className="ml-8 list-disc">
-                <TestAppLink href="https://maxzz.github.io/test-pm" label="Five customizable logins and password change screens" />
+                <TestAppLink label="Five customizable logins and password change screens" href="https://maxzz.github.io/test-pm" />
             </ul>
             <img className="pt-3 m-auto bg-slate-300" width="200px" src={sampleAppsImg} alt="test applications preview" />
 
             {/* Page reload test */}
             <br />
             <ul className="ml-8 list-disc">
-                <TestAppLink href="https://maxzz.github.io/test-pm-domain-logins22" label="Page reload test with login and corresponding password change screens on the same domain" />
+                <TestAppLink label="Page reload test with login and corresponding password change screens on the same domain" href="https://maxzz.github.io/test-pm-domain-logins22" />
             </ul>
             <img className="pt-3 m-auto" width="200px" src={sampleApps22Img} alt="test applications preview" />
 
             {/* PMIT */}
             <br />
             <ul className="ml-8 list-disc">
-                <TestAppLink href={URLS.HID_PMIT} label="Password Manager Investigation Tool (version under HID domain)" />
-                <TestAppLink href="https://maxzz.github.io/dropzone" label="Password Manager Investigation Tool (the latest version)" />
+                <TestAppLink label="Password Manager Investigation Tool (version under HID domain)" href={URLS.HID_PMIT} />
+                <TestAppLink label="Password Manager Investigation Tool (the latest version)" href="https://maxzz.github.io/dropzone" />
             </ul>
             <img className="pt-3 m-auto" width="200px" src={samplePmitImg} alt="test applications preview" />
 
@@ -58,15 +58,15 @@ export function Section4_TestApps() {
             <br />
             <div className="">Local test pages</div>
             <ul className="ml-8 list-disc">
-                <TestAppLink href={URLS.LOCAL_APP_FISERV} label={<> Bug &#8470; 90673 (fiserv:Heritage Bank), Bug &#8470; 91149 (fiserv:BAC Bank)</>} />
-                <TestAppLink href={URLS.LOCAL_APP_BANK_UTICA} label={<>Bug &#8470; 91370 (Bank of Utica)</>} />
-                <TestAppLink href={URLS.LOCAL_APP_BANK_WASHINGTON} label={<>Bug &#8470; 91506 (Bank of Washington)</>} />
+                <TestAppLink label={<> Bug &#8470; 90673 (fiserv:Heritage Bank), Bug &#8470; 91149 (fiserv:BAC Bank)</>} href={URLS.LOCAL_APP_FISERV} />
+                <TestAppLink label={<>Bug &#8470; 91370 (Bank of Utica)</>} href={URLS.LOCAL_APP_BANK_UTICA} />
+                <TestAppLink label={<>Bug &#8470; 91506 (Bank of Washington)</>} href={URLS.LOCAL_APP_BANK_WASHINGTON} />
             </ul>
 
             {/*  */}
             <br />
             <ul className="ml-8 list-disc">
-                <TestAppLink href="https://maxzz.github.io" label="All other projects catalog" />
+                <TestAppLink label="All other projects catalog" href="https://maxzz.github.io" />
             </ul>
 
             <ul className="ml-8 list-disc">
